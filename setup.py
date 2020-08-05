4ce1b7dc498271a8abfccede257e548c2b395653
sumner-harris <69218581+sumner-harris@users.noreply.github.com>
2020-08-05 14:42:09 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+  
+from distutils.core import setup
+
+setup(
+    name='pricescryer',
+    version='0.1',
+    packages=['pricescryer',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    url='https://github.com/sumner-harris/pricescryer/'
+)
