e5553f88b8b80e42f93aca68970b9326a1008490
sumner-harris <69218581+sumner-harris@users.noreply.github.com>
2020-08-05 14:43:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from distutils.core import setup
 
 setup(
-    name='pricescryer',
+    name='plasmaslicer',
     version='0.1',
-    packages=['pricescryer',],
+    packages=['plasmaslicer',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
-    url='https://github.com/sumner-harris/pricescryer/'
+    url='https://github.com/sumner-harris/plasmaslicer/'
 )
